7fef57ca70e734da173d9d35a69267c6b85175db
Darron Froese <darron@froese.org>
2013-10-24 14:00:25 -0600

    Moving config files to repo.

--- scripts/serf.sh
+++ scripts/serf.sh
@@ -4,10 +4,10 @@ cd /tmp
 wget https://dl.bintray.com/mitchellh/serf/0.1.1_linux_amd64.zip
 unzip 0.1*
 sudo mv serf /usr/local/bin
-wget https://gist.github.com/darron/7126986/raw/c023b0b7bc48c8a1f7ce52f065d38b0e70d42aba/serf.conf
+wget https://raw.github.com/darron/packer-ubuntu-13.10-docker-serf/master/config/serf.conf
 sudo mv serf.conf /etc/init/
-wget https://gist.github.com/darron/7142708/raw/1475e10d9018ba2a52083e4b041c247fac24ae5f/serf.default
+wget https://raw.github.com/darron/packer-ubuntu-13.10-docker-serf/master/config/serf.default
 sudo mv serf.default /etc/default/serf
 cd /etc/init.d; sudo ln -s /lib/init/upstart-job serf
-cd /etc/logrotate.d/; sudo wget https://gist.github.com/darron/7129312/raw/9d0bef1876b9fbe0eed4fc07ed0a0c7dcc8a8c50/serf
+cd /etc/logrotate.d/; sudo wget https://raw.github.com/darron/packer-ubuntu-13.10-docker-serf/master/config/serf.logrotate; sudo mv serf.logrotate serf
 sudo service serf start
