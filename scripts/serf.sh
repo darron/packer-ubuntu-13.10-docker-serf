f69d255c0dc5b62469feb1b448e560900018fb82
Darron Froese <darron@froese.org>
2013-10-24 13:18:56 -0600

    Updating a few scripts.

--- scripts/serf.sh
+++ scripts/serf.sh
@@ -4,8 +4,10 @@ cd /tmp
 wget https://dl.bintray.com/mitchellh/serf/0.1.1_linux_amd64.zip
 unzip 0.1*
 sudo mv serf /usr/local/bin
-wget https://gist.github.com/darron/7126986/raw/8830fbfe160d3b17cf431ee9931afb4d1f0122ad/serf.conf
+wget https://gist.github.com/darron/7126986/raw/c023b0b7bc48c8a1f7ce52f065d38b0e70d42aba/serf.conf
 sudo mv serf.conf /etc/init/
+wget https://gist.github.com/darron/7142708/raw/1475e10d9018ba2a52083e4b041c247fac24ae5f/serf.default
+sudo mv serf.default /etc/default/serf
 cd /etc/init.d; sudo ln -s /lib/init/upstart-job serf
 cd /etc/logrotate.d/; sudo wget https://gist.github.com/darron/7129312/raw/9d0bef1876b9fbe0eed4fc07ed0a0c7dcc8a8c50/serf
 sudo service serf start
