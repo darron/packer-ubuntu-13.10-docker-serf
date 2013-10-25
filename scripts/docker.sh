0bba57e6a6c8998ddd09f03c22065c7d388abd61
Darron Froese <darron@froese.org>
2013-10-24 18:11:40 -0600

    Try again to install Git.

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Add the Docker repository to your apt sources list.
-sudo apt-get install curl wget
+sudo apt-get -y install curl wget git
 sudo sh -c "wget -qO- https://get.docker.io/gpg | apt-key add -"
 sudo sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,4 +3,3 @@
 # Update all packages.
 sudo apt-get update
 sudo apt-get -y upgrade
-# sudo apt-get -y install git
