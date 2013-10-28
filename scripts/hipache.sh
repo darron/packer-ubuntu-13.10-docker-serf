e7dd0d3fbad1ee6f570db52966987a4f313f1830
Darron Froese <darron@froese.org>
2013-10-28 16:47:02 -0600

    Forked my own copy and fixed the broken packages.json file.

--- scripts/hipache.sh
+++ scripts/hipache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get -y install nodejs npm redis-server
-sudo npm install hipache -g
+sudo npm install https://github.com/darron/hipache/archive/0.2.4.tar.gz -g
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 sudo mkdir /etc/hipache
 cd /etc/hipache; sudo wget https://raw.github.com/darron/packer-ubuntu-13.10-docker-serf/master/config/hipache.json
